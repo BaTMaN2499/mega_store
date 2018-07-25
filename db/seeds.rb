e4920102229a8ed325af5b2f1830c54c82c30480
victor-am <victor.atmorning@gmail.com>
2018-07-25 02:09:23 -0300

    Add some basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "\nSeeding database...\n\n"
+
+puts '- Creating users...'
+Customer.create!(
+  {
+    name: 'John Snow',
+    address: 'Winterfell Castle, North of Westeros'
+  }
+)
+
+puts '- Creating stores...'
+Store.create!([
+  {
+    name: 'Amazon',
+    address: 'Amazon.com, Inc. Customer Service PO Box 81226 Seattle, WA 98108-1226'
+  },
+  {
+    name: 'Submarino',
+    address: 'Sacadura Cabral Street, 102 - Rio de Janeiro, RJ 20081-902'
+  },
+  {
+    name: 'eBay',
+    address: '2065 Hamilton Avenue. San Jose, California 95125'
+  }
+])
+
+puts '- Creating products...'
+Product.create!([
+  {
+    name: 'Candy Is Magic: Real Ingredients, Modern Recipe',
+    base_price: '52.00',
+    stock: 52,
+    store: Store.find_by(name: 'Amazon')
+  }
+])
+
+puts "\nDone!"
