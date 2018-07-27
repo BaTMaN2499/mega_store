819a4aadb2517c20a32d31644e53f325a5337444
victor-am <victor.atmorning@gmail.com>
2018-07-27 19:31:47 -0300

    Improve seeds with idempotency

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 puts "\nSeeding database...\n\n"
 
 puts '- Creating users...'
-Customer.create!(
+customers = [
   {
     name: 'John Snow',
     address: 'Winterfell Castle, North of Westeros'
   }
-)
+]
+customers.each{ |attrs| Customer.find_or_create_by!(attrs) }
 
 puts '- Creating stores...'
-Store.create!([
+stores = [
   {
     name: 'Amazon',
     address: 'Amazon.com, Inc. Customer Service PO Box 81226 Seattle, WA 98108-1226'
@@ -30,16 +26,18 @@ Store.create!([
     name: 'eBay',
     address: '2065 Hamilton Avenue. San Jose, California 95125'
   }
-])
+]
+stores.each{ |attrs| Store.find_or_create_by!(attrs) }
 
 puts '- Creating products...'
-Product.create!([
+products = [
   {
     name: 'Candy Is Magic: Real Ingredients, Modern Recipe',
     base_price: '52.00',
     stock: 52,
-    store: Store.find_by(name: 'Amazon')
+    store: Store.find_by!(name: 'Amazon')
   }
-])
+]
+products.each{ |attrs| Product.find_or_create_by!(attrs) }
 
 puts "\nDone!"
