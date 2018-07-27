86fb020ea68c86dcb48a527de483978304c26449
victor-am <victor.atmorning@gmail.com>
2018-07-27 19:42:29 -0300

    Replace random doc with Tomdoc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::API
-  #
-  # This method simulates an authorization feature
-  #
+  # Public: This method simulates an authorization feature
   def current_user
     Customer.first
   end
